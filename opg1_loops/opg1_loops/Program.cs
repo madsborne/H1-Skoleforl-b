4fb549b5ae6e16918f9ab8c1f54897681f1eabbf
Mads Borne <36690649+madsborne@users.noreply.github.com>
2019-01-26 19:33:54 +0100

    løkke opgave

---
+++ opg1_loops/opg1_loops/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opg1_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a for loop counting to 100 if it's below 50 it will print it to the console when it goes above it won't write anything to the console
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 51)
+                {
+                    Console.WriteLine(i); 
+                }
+            }
+        }
+    }
+}
