61420668e5e4035d7b5300a8c09263cd19d13917
Mads Borne Sparre <mads648i@zbc.dk>
2019-03-07 12:30:01 +0100

    Added WashingMachine opgave

---
+++ WasherOOP/WasherOOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // keeps the program running
+            while (true)
+            {
+                // creating a new washing machine and naming it machine
+                WashingMachine machine = new WashingMachine();
+
+                // asks the user what washing mode they wanna run
+                Console.WriteLine("What washing mode do you wanna select?");
+                Console.WriteLine("1: Eco mode ");
+                Console.WriteLine("2: Normal mode ");
+
+                // runs the machine.Menu method 
+                machine.Menu(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ WasherOOP/WasherOOP/WashingMachine.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WasherOOP
+{
+    class WashingMachine
+    {
+        // a public method that runs the programs menu
+        public void Menu(string userInput)
+        {
+            // controls the users input
+            switch (userInput.ToLower())
+            {
+                case "1":
+                    Console.WriteLine("have you closed the door?");
+                    Start("eco", Console.ReadLine());
+                    break;
+                case "2":
+                    Console.WriteLine("have to closed the door?");
+                    Start("normal", Console.ReadLine());
+                    break;
+                default:
+                    Console.WriteLine("not a valid mode");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// a method that starts the program depending on the user input
+        /// </summary>
+        /// <param name="userInputWashMode"></param>
+        /// <param name="userDoorStatus"></param>
+        public void Start(string userInputWashMode, string userDoorStatus)
+        {
+            if (userDoorStatus.ToLower() == "yes" && userInputWashMode == "eco")
+            {
+                Console.WriteLine("Running eco mode");
+            }
+            else if (userDoorStatus.ToLower() == "yes" && userInputWashMode == "normal")
+            {
+                Console.WriteLine("Running normal mode");
+            }
+            else
+            {
+                Console.WriteLine("you need to close the door first");
+            }
+        }
+
+
+    }
+}
