1d8fe9a9bdd964c9c894628dd2f0c90a9c5f90b5
Mads Borne Sparre <mads648i@zbc.dk>
2019-03-11 13:10:49 +0100

    added OopPrimeNumbers opgave

---
+++ OopPrimeNumbers/OopPrimeNumbers/QueueControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopPrimeNumbers
+{
+    class QueueControl
+    {
+        Queue<int> primeNumbers = new Queue<int>();
+
+        public QueueControl()
+        {
+            primeNumbers.Enqueue(2);
+            primeNumbers.Enqueue(3);
+            primeNumbers.Enqueue(5);
+            primeNumbers.Enqueue(7);
+            primeNumbers.Enqueue(11);
+        }
+
+        public void PrimeNumberPrint()
+        {
+            while (primeNumbers.Count > 0)
+            {
+                int number = primeNumbers.Dequeue();
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
