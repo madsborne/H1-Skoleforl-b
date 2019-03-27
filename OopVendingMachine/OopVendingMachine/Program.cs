b571cccb95b5a3e839f77d9837ffde055f523afc
Mads Borne <36690649+madsborne@users.noreply.github.com>
2019-03-27 18:26:56 +0100

    finished the vending machine

--- OopVendingMachine/OopVendingMachine/Program.cs
+++ OopVendingMachine/OopVendingMachine/Program.cs
@@ -10,14 +10,63 @@ namespace OopVendingMachine
     {
         static void Main(string[] args)
         {
+            // creates the vendingmachine
             VendingMachine vending = new VendingMachine();
 
+            // vars
+            bool poweringOn = true;
+            bool adminLogin = false;
             string loginPass = "";
+            string userInput = "";
+            int timer = 0;
+            int productID = 0;
             int userMoney = 0;
 
+            // keeps the program running
             while (true)
             {
-                
+                // greets the user and asks for a input if the input is adminadminadmin the user is then taken into the admin menu 
+                // also prints the products in the machine
+                Console.WriteLine("Welcome to this Vending Machine");
+                Console.Write(vending.PrintProducts());
+                userInput = Console.ReadLine();
+                if (userInput == "adminadminadmin")
+                {
+                    // local vars
+                    string localUserInput = "";
+                    string localUserMoney = "";
+
+                    // prints the admin menu
+                    Console.WriteLine(vending.PrintAdminMenu());
+                    ConsoleKeyInfo charUserInput = Console.ReadKey();
+
+                    // try parses the userinput to check if it is a int
+                    Int32.TryParse(localUserInput, out productID);
+                    
+                    // tries to run the vending menu
+                    try
+                    {
+                        Console.WriteLine(vending.VendingMenu(charUserInput.KeyChar, productID, 20, userInput));
+                        vending.FillProduct();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                // runes the normal menu
+                else
+                {
+                    Int32.TryParse(userInput, out productID);
+                    Console.Clear();
+                    Console.WriteLine(vending.PrintOneProduct(productID));
+                    Console.WriteLine("Input money!");
+                    userInput = Console.ReadLine();
+                    Int32.TryParse(userInput, out userMoney);
+                    Console.WriteLine(vending.PushProduct(productID, userMoney));
+                    Console.ReadKey();
+                    Console.Clear(); 
+                }
             }
             
         }
--- OopVendingMachine/OopVendingMachine/VendingMachine.cs
+++ OopVendingMachine/OopVendingMachine/VendingMachine.cs
@@ -8,15 +8,18 @@ namespace OopVendingMachine
 {
     class VendingMachine
     {
+        // stacks and lists
         private Stack<Product> slots;
         private List<Stack<Product>> vendingSlots = new List<Stack<Product>>();
         private List<Candy> removedCandies = new List<Candy>();
         private List<Drink> removedDrinks = new List<Drink>();
 
+        // uninstanced objects
         private Candy candy;
         private Drink drink;
         private Product temp;
 
+        // atti
         private int moneyBox = 0;
         private int returnMoney = 0;
         private bool buyComplete = false;
@@ -27,13 +30,22 @@ namespace OopVendingMachine
         /// </summary>
         public VendingMachine()
         {
+            for (int i = 0; i < 6; i++)
+            {
+                vendingSlots.Add(slots = new Stack<Product>());
+            }
 
             FillProduct();
         }
 
+        /// <summary>
+        /// takes in userMoney if usermoney is greater then price it returns the rest and turns to buycomplete to true
+        /// </summary>
+        /// <param name="userMoney"></param>
+        /// <returns></returns>
         private int CoinInsert(int userMoney)
         {
-            if (userMoney > temp.Price)
+            if (userMoney >= temp.Price)
             {
                 returnMoney = userMoney -= temp.Price;
                 moneyBox += temp.Price;
@@ -48,12 +60,21 @@ namespace OopVendingMachine
             
         }
 
+        /// <summary>
+        /// lets the user pick a product from the id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         private int PickProduct(int id)
         {
             temp = vendingSlots[id].Pop();
             return temp.Price;
         }
 
+        /// <summary>
+        /// checks the vendingmachines stash to see if it full or not 
+        /// </summary>
+        /// <returns></returns>
         private bool CountCheck()
         {
             int productCount = 0;
@@ -75,7 +96,13 @@ namespace OopVendingMachine
             }
         }
 
-        private string PushProduct(int productNumber, int userMoney)
+        /// <summary>
+        /// if the buy was complete in CoinInsert this method runs and pushes the product to the user
+        /// </summary>
+        /// <param name="productNumber"></param>
+        /// <param name="userMoney"></param>
+        /// <returns></returns>
+        public string PushProduct(int productNumber, int userMoney)
         {
             PickProduct(productNumber);
             CoinInsert(userMoney);
@@ -104,14 +131,12 @@ namespace OopVendingMachine
 
         }
 
-        private void FillProduct()
+        /// <summary>
+        /// first empties the machine and restock it
+        /// </summary>
+        public void FillProduct()
         {
-
-            for (int i = 0; i < 6; i++)
-            {
-                vendingSlots.Add(slots = new Stack<Product>());
-            }
-
+            
             EmptyVending();
 
             for (int i = 0; i < vendingSlots.Count; i++)
@@ -119,37 +144,37 @@ namespace OopVendingMachine
                 switch (i)
                 {
                     case 0:
-                        for (int addingFoamBanana = 0; addingFoamBanana < vendingSlots.Count; addingFoamBanana++)
+                        for (int addingFoamBanana = 0; addingFoamBanana < 5; addingFoamBanana++)
                         {
                             vendingSlots[0].Push(candy = new Candy("Foam Banana", 5, "xl"));
                         }
                         break;
                     case 1:
-                        for (int addingFoamPlatyPus = 0; addingFoamPlatyPus < vendingSlots.Count; addingFoamPlatyPus++)
+                        for (int addingFoamPlatyPus = 0; addingFoamPlatyPus < 5; addingFoamPlatyPus++)
                         {
                             vendingSlots[1].Push(candy = new Candy("Foam Platypus", 10, "xxl"));
                         }
                         break;
                     case 2:
-                        for (int addingFoamDuck = 0; addingFoamDuck < vendingSlots.Count; addingFoamDuck++)
+                        for (int addingFoamDuck = 0; addingFoamDuck < 5; addingFoamDuck++)
                         {
                             vendingSlots[2].Push(candy = new Candy("Foam Duck", 15, "xl"));
                         }
                         break;
                     case 3:
-                        for (int addingCola = 0; addingCola < vendingSlots.Count; addingCola++)
+                        for (int addingCola = 0; addingCola < 5; addingCola++)
                         {
                             vendingSlots[3].Push(drink = new Drink("Cola", 15, 1));
                         }
                         break;
                     case 4:
-                        for (int addingFanta = 0; addingFanta < vendingSlots.Count; addingFanta++)
+                        for (int addingFanta = 0; addingFanta < 5; addingFanta++)
                         {
                             vendingSlots[4].Push(drink = new Drink("Fanta", 15, 1));
                         }
                         break;
                     case 5:
-                        for (int addingFoamDuckDrink = 0; addingFoamDuckDrink < vendingSlots.Count; addingFoamDuckDrink++)
+                        for (int addingFoamDuckDrink = 0; addingFoamDuckDrink < 5; addingFoamDuckDrink++)
                         {
                             vendingSlots[5].Push(drink = new Drink("Liqid Foam Drink", 20, 0.5));
                         }
@@ -163,14 +188,24 @@ namespace OopVendingMachine
 
         }
 
+        /// <summary>
+        /// Empties the machine
+        /// </summary>
         private void EmptyVending()
         {
             for (int i = 0; i < vendingSlots.Count; i++)
             {
                 vendingSlots[i].Clear();
             }
+            
         }
 
+        /// <summary>
+        /// changes the price and lets the user know what item and the price it has been changed to
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="newPrice"></param>
+        /// <returns></returns>
         private string ChangePrice(int product, int newPrice)
         {
             string tempName = "";
@@ -180,20 +215,26 @@ namespace OopVendingMachine
                 item.Price = newPrice;
                 tempName = item.Name;
             }
-
+            
             return $"The price of {tempName} has been changed to {newPrice}";
-        }
 
-        private string VendingMenu(char userInput, int productId, int userMoney, string admin)
+        }
+        
+        /// <summary>
+        /// admin menu lets the admin refill the machine empty it and change price
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <param name="productId"></param>
+        /// <param name="userMoney"></param>
+        /// <param name="admin"></param>
+        /// <returns></returns>
+        public string VendingMenu(char userInput, int productId, int userMoney, string admin)
         {
             if (admin == "adminadminadmin")
             {
                 switch (char.ToLower(userInput))
                 {
-                    case '0':
-                        returnString = PushProduct(productId, userMoney);
-                        break;
-
+                    
                     case 'x':
                         if (CountCheck() == false)
                         {
@@ -211,8 +252,13 @@ namespace OopVendingMachine
                         EmptyVending();
                         returnString = "Machine has been emptyed";
                         break;
+
                     case 'c':
-                        ChangePrice(productId, userMoney);
+                        returnString = ChangePrice(productId, userMoney);
+                        break;
+
+                    case 'p':
+                        returnString = PrintProducts();
                         break;
 
                     default:
@@ -228,18 +274,48 @@ namespace OopVendingMachine
             return returnString;
         }
 
+        /// <summary>
+        /// prints the products in the machine
+        /// </summary>
+        /// <returns></returns>
         public string PrintProducts()
         {
             returnString = "";
 
             for (int i = 0; i < vendingSlots.Count; i++)
             {
-                returnString = $"ID - {i} \n" +
-                    $"Name - {vendingSlots[i].Peek().Name}\n" +
-                    $"P"; 
+                returnString += $"\nID - {i} \n" +
+                    $"Name  - {vendingSlots[i].Peek().Name}\n" +
+                    $"Price - {vendingSlots[i].Peek().Price}\n"; 
             }
 
-            return "";
+            return returnString + "\nInput ID : ";
+        }
+
+        /// <summary>
+        /// only prints one product
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        public string PrintOneProduct(int productID)
+        {
+            returnString = $"Name = {vendingSlots[productID].Peek().Name}.\n" +
+                           $"Price = {vendingSlots[productID].Peek().Price}.";
+            return returnString;
+        }
+
+        /// <summary>
+        /// prints the admin menu
+        /// </summary>
+        /// <returns></returns>
+        public string PrintAdminMenu()
+        {
+
+            return "Admin Menu  \n" +
+                   "Press [X] to fill vending machine.\n" +
+                   "Press [Z] to empty machine.\n" +
+                   "Press [C] to change price.\n" +
+                   "Press [P] to print products.\n";
         }
     }
 }
